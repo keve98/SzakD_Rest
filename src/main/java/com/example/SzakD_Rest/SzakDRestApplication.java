456cc31a9f77a6b8be36e7a5d767e1f80598b09d
keve98 <61545736+keve98@users.noreply.github.com>
2021-10-05 14:22:35 +0200

    JSF frontend pages configured

--- src/main/java/com/example/SzakD_Rest/SzakDRestApplication.java
+++ src/main/java/com/example/SzakD_Rest/SzakDRestApplication.java
@@ -2,12 +2,31 @@ package com.example.SzakD_Rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SzakDRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SzakDRestApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
+		//spring boot only works if this is set
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+		//FacesServlet registration
+		ServletRegistrationBean srb = new ServletRegistrationBean();
+		srb.setServlet(new FacesServlet());
+		srb.setUrlMappings(Arrays.asList("*.xhtml"));
+		srb.setLoadOnStartup(1);
+		return srb;
 	}
 
 }
